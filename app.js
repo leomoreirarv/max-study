bbb1fa069c0e250b7e760f2305325e7a79b144fa
Leonardo Moreira <leonardo.moreira@fleetmatics.com>
2018-01-06 13:46:56 +0000

    started project

---
+++ app.js
@@ -0,0 +1,21 @@
+// (function(){
+//     "use strict";
+
+var Validation = function(fieldId){
+    this.fieldId = fieldId;
+}
+
+Validation.prototype.init = function(){
+  var val = document.getElementById(this.fieldId).value;
+  console.log(val);
+}
+
+Validation.prototype.min = function(mincaracters){
+    var val = document.getElementById(this.fieldId).value;
+    if(val.length < mincaracters){
+        console.log("Ops, you must insert minimum " + mincaracters + " caracters");
+    } else {
+        console.log("Ok, validate");
+    }   
+}
+// })();

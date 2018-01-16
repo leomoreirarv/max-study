dd07cba8343e6b253ec641a9c608ec4743cf5df3
makireland <makireland@gmail.com>
2018-01-16 20:17:37 +0000

    criacao do metodo isEuqal e melhoria do metodo message criando span

--- app.js
+++ app.js
@@ -7,33 +7,50 @@ Validation.prototype.init = function () {
     console.log(val);
 }
 
-Validation.prototype.min = function (mincaracters) {
+Validation.prototype.min = function (mincharacters) {
     var val = document.getElementById(this.fieldId).value;
-    if (val.length < mincaracters) {
-        this.msg("Ops, you must insert minimum " + mincaracters + " caracters");
+    if (val.length < mincharacters) {
+        this.msg("Ops, you must insert minimum " + mincharacters + " characters");
     } else {
-        console.log("min","Ok, validate");
+        console.log("min", "Ok, validate");
     }
 }
 
 Validation.prototype.empty = function () {
     var val = document.getElementById(this.fieldId).value;
     if (val === "") {
-        this.msg("Por favor preencha os campos");
+        this.msg("The Field cannot be empty");
     } else {
-        console.log("empty","Ok, validate");
+        console.log("empty", "Ok, validate");
     }
 }
 
-Validation.prototype.max = function (maxcaracters) {
+Validation.prototype.max = function (maxcharacters) {
     var val = document.getElementById(this.fieldId).value;
-    if (val.length > maxcaracters) {
-       this.msg("por favor preencha menos de " +maxcaracters+ " caracters ");
+    if (val.length > maxcharacters) {
+        this.msg("You must insert the maximun " + maxcharacters + " of characters ");
     } else {
-        console.log("max","Ok, validate");
+        console.log("max", "Ok, validate");
     }
+
 }
 
-Validation.prototype.msg = function (menssage) {
-    alert(this.fieldId +" : "+ menssage); 
+Validation.prototype.msg = function (message) {
+    var span = document.createElement("span");
+    span.innerText= message;
+    var div = document.getElementById(this.fieldId).parentElement;
+    div.appendChild(span);
+    console.log(div);
+    //alert(this.fieldId + " : " + message);
+
+}
+
+Validation.prototype.isEqual = function (field) {
+    var val = document.getElementById(this.fieldId).value;
+    var val2 = document.getElementById(field).value;
+    if (val !== val2) {
+        this.msg(this.fieldId + " is not matching " + field + " with field2 ");
+    } else {
+        console.log(" It is Equal "," ok Validated ");
+    }
 }
